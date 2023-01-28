42ebd7201e5ab3eb7952b9f15b5b6da9d81c2c45
Jimmy Blundell <jimmy.blundell@agisuretrack.com>
2023-01-27 22:36:48 -0600

    clean up login page, get states to working

--- frontend/rss-feed/src/App.tsx
+++ frontend/rss-feed/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import { ReactComponent as Logo } from "./logo.svg";
 import { getData } from "./utils/data-utils";
 import FormInput from './components/form-input/form-input';
@@ -8,70 +8,111 @@ import './App.css';
 // TypeScript declarations
 type User = {
     id: number,
-    name: string,
-    email: string,
+    username: string,
     password: string
 }
 
-const defaultFormFields = {
-    email: '',
-    password: '',
-}
-
 const App = () => {
-    // react hooks
-    const [user, setUser] = useState<User | null>()
-    const [formFields, setFormFields] = useState(defaultFormFields)
-    const { email, password } = formFields
+    const [user, setUser] = useState<User | null>();
+    const [emailReg, setEmailReg] = useState('');
+    const [passwordReg, setPasswordReg] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [password, setPassword] = useState('');
 
-    const resetFormFields = () => {
-        return (
-            setFormFields(defaultFormFields)
-        );
-    }
 
-    // handle input changes
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target
-        setFormFields({...formFields, [name]: value })
-    }
-
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    const handleLoginSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
 
         try {
-            // make the API call
             const res:User = await getData(
                 'http://localhost:8000/login', email, password
             )
             setUser(res);
-            resetFormFields()
+            // resetLoginFields()
         } catch (error) {
             alert('User Sign In Failed');
         }
     };
 
-    const reload = () => {
-        setUser(null);
-        resetFormFields()
+    const handleRegisterSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        try {
+            const res:User = await getData(
+                'http://localhost:8000/login', emailReg, passwordReg
+            )
+            setUser(res);
+            // resetRegisterFields()
+        } catch (error) {
+            alert('User Register Failed');
+        }
     };
+
+    const resetRegistration = () => {
+        setEmailReg('');
+        setPasswordReg('');
+    }
+    const resetLogin = () => {
+        setEmail('');
+        setPassword('');
+    }
 
     return (
         <div className='App-header'>
             <h1>
-                { user && `Welcome! ${user.name}`}
+                { user && `Welcome! ${user.username}`}
             </h1>
             <div className="card">
                 <Logo className="logo" />
+                <h2>Register</h2>
+                <form onSubmit={handleRegisterSubmit} id={"registerForm"}>
+                    <FormInput
+                        label="Email"
+                        type="email"
+                        required
+                        name="email"
+                        value={emailReg}
+                        onChange={(e) => {
+                            setEmailReg(e.target.value);
+                        }}
+                        id={"registerEmail"}
+                    />
+                    <FormInput
+                        label="Password"
+                        type='password'
+                        required
+                        name='password'
+                        value={passwordReg}
+                        onChange={(e) => {
+                            setPasswordReg(e.target.value);
+                        }}
+                        id={"registerPassword"}
+                    />
+                    <div className="button-group">
+                        <button type="submit">Register</button>
+                        <span>
+                <button type="button" onClick={resetRegistration}>Clear</button>
+              </span>
+                    </div>
+                </form>
+            </div>
+            <br/>
+            <h4> OR </h4>
+            <br/>
+            <div className="card">
+                <Logo className="logo" />
                 <h2>Sign In</h2>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleLoginSubmit} id={"signInForm"}>
                     <FormInput
                         label="Email"
                         type="email"
                         required
                         name="email"
                         value={email}
-                        onChange={handleChange}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        id={"signInEmail"}
                     />
                     <FormInput
                         label="Password"
@@ -79,12 +120,15 @@ const App = () => {
                         required
                         name='password'
                         value={password}
-                        onChange={handleChange}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                        id={"signInPassword"}
                     />
                     <div className="button-group">
                         <button type="submit">Sign In</button>
                         <span>
-                <button type="button" onClick={reload}>Clear</button>
+                <button type="button" onClick={resetLogin}>Clear</button>
               </span>
                     </div>
                 </form>
